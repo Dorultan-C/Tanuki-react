bc2dbc86b936c27e579df29e7d62c3c66f8abac1
Cristian Dorultan <c.dorultan@gmail.com>
2023-04-03 00:06:54 +0100

    conditional statement

--- src/App.js
+++ src/App.js
@@ -2,45 +2,15 @@
 import './App.css';
 
 function App() {
-  const welcomeMessage = <h1>Hello Mr.</h1>
-  const name = <h1>Christian</h1>;
+  const age = 115;
+  const isColor = true;
   return (
-    <div className="App"> {welcomeMessage} {name}
-     <User age={31} email='lala.com' />
-     <Job salary='£120.000' position='Software engineer' company='Google' />
-     <Passion flower='rose' color='indigo' cars='tesla' />
-     </div>
+    <div className="App">
+      {age >= 18 ? <h1>over age</h1> : <h1>under age</h1> }
+      <h1 style={{color: isColor ? 'green' : 'red'}}>this is sparta</h1>
+      {isColor && <button>tap to tap</button>}
+    </div>
     
   );
-}
-
-const Passion = (props) => {
-  return (
-    <div className='passion'>
-      <p> {props.flower} </p>
-      <p> {props.color} </p>
-      <p> {props.cars} </p>
-    </div>
-  )
-};
-const Job = (props) => {
-  return (
-    <div>
-       <h2> {props.salary} </h2>
-       <h2> {props.position} </h2>
-       <h2> {props.company} </h2>
-    </div>
-  )
-};
-
-const User = (props) => {
-  return (
-   <div>
-    <h1> {props.name} </h1>
-    <h1> {props.age} </h1>
-    <h1> {props.email} </h1>
-   </div>
-  )
-};
-
+} 
 export default App;
