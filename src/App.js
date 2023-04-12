b548a294098ab8c0111f81b39b5a3c77e8b3dd60
Cristian Dorultan <c.dorultan@gmail.com>
2023-04-12 16:23:00 +0100

    new clean start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 
-import './App.css';
-
 function App() {
-  const age = 115;
-  const isColor = true;
+
   return (
-    <div className="App">
-      {age >= 18 ? <h1>over age</h1> : <h1>under age</h1> }
-      <h1 style={{color: isColor ? 'green' : 'red'}}>this is sparta</h1>
-      {isColor && <button>tap to tap</button>}
-    </div>
-    
+    <h1>hi</h1>
   );
 } 
 export default App;
