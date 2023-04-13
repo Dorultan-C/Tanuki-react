cece1c0d087e4007e4da34909653dde608768b92
Cristian Dorultan <c.dorultan@gmail.com>
2023-04-13 10:41:03 +0100

    structure for nav menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import '../src/components/Header';
 import Header from '../src/components/Header';
 import '../src/components/scss/Main.scss';
+import './components/Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div>
       <Header/>
+      <Navbar/>
       
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className='navigation-menu'>
+        <li>Figurine</li>
+        <li>Anime</li>
+        <li>Manga</li>
+        <li>Movies</li>
+        <li>Gaming</li>
+        <li>Pokemon</li>
+      </ul>
+    </nav>
+  )
+}
