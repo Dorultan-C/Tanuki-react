0913f69c839ee5cc78e059d7a8485eca351a48ed
Cristian Dorultan <c.dorultan@gmail.com>
2023-04-13 09:31:32 +0100

    tanuki logo and carpet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PhoneIcon from './photos/phone.svg';
+import TanukiLogo from './photos/tanuki-logo.svg';
+import TanukiCarpet from './photos/tanuki-background-carpet.png';
 
 export default function Header() {
   return (
-    <div className='contact-header'>
-      <a href="https://wa.me/+4407462971007" target="_blank">
-      <img
-        src= {PhoneIcon} >
-      </img>
-      <p> 07462971007</p>
-      </a>
+    <div>
+      <div className='contact-header'>
+          <a href="https://wa.me/+4407462971007" target="_blank">
+            <img className='phone-logo' src= {PhoneIcon}></img>
+            Contact us: 07462971007 
+          </a>
+      </div>
+
+      <div >
+        <img className='logo-header' src={TanukiLogo}></img>
+       
+      </div>
+      <div >
+         <img className='tanuki-carpet' src={TanukiCarpet} ></img>
+      </div>
+
     </div>
   )
 }
